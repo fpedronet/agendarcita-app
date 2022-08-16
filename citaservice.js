cda8171055e46e51bdd1b194e48d7088ad1a7e69
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-08-16 16:45:43 -0500

    merge

---
+++ citaservice.js
@@ -0,0 +1,51 @@
+onload=function(){
+    var ejemplo = 'https://t3.ftcdn.net/jpg/04/19/17/68/360_F_419176802_9s4AoYMfzxDt3kaSYV55whCkTB76NsHN.jpg'
+    /*Imágenes*/
+    document.getElementById("logoCampana").src = ejemplo;
+}
+
+const button1 = document.getElementById("btnListar");
+const button2 = document.getElementById("btnAgendar");
+
+button1.addEventListener('click', () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => (res.ok? Promise.resolve(res): Promise.reject(res)))
+    .then((res) => res.json(res))
+    .then((res) => console.log(res));
+
+});
+
+button2.addEventListener('click', () => {
+
+    const objct ={
+        /*vNombre:"FRANCISCO PEDRO",
+        vApellido:"CONDOR MARTINEZ",
+        vDocumento:"45036143",
+        dFechaNac:"1988-03-01",
+        vCelular:"965839805",
+        vCorreo:"fpedro.martinez.net@gmail.com",
+        dDia:"2022-08-16",
+        vLugar:"lima",*/
+        vNombre: document.getElementById('nombres').value,
+        vApellido: document.getElementById('apellidos').value,
+        vDocumento: document.getElementById('dni').value,
+        dFechaNac: document.getElementById('fecnacimiento').value,
+        vCelular: document.getElementById('celular').value,
+        vCorreo: document.getElementById('correo').value,
+        dDia: document.getElementById('dia').value,
+        vLugar: document.getElementById('lugar').value,
+        dFechaReg:"2022-08-16",
+        Key:"!SDFT$$$$&F(/GF7&F7f))?=0'===IY(&&%$%$!H(U/GFD%VBN(MI YT% %RCGRCVBBUJNU(NN"
+    }
+
+    fetch("https://service.poclab.pe/agendarcita/api/cita/PostAgendarCita", {
+        method: "POST",
+        body: JSON.stringify(objct),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((res) => res.json())
+    .then((response) => console.log("Exito: ", response))
+    .catch((error) => console.log("Error: ", error));
+});
