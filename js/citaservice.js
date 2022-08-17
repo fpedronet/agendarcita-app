ae820cdfcbb30ea896a1fd506b804578c0fcca9f
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-08-17 15:00:05 -0500

    merge

--- js/citaservice.js
+++ js/citaservice.js
@@ -1,3 +1,5 @@
+var $aniodesde = 0;
+
 $(document).ready(function(){
 
     $('#datetimepicker12').datepicker({
@@ -14,6 +16,8 @@ $(document).ready(function(){
     var count = 1;
     var aniohasta = anio - 18
     var aniodesde = anio - 65
+
+    $aniodesde = aniodesde;
 
     $("#cbdia").html("");
     $("#cbmeses").html("");
@@ -37,15 +41,54 @@ $(document).ready(function(){
         $("#cbanio").html(itemAnio);
     }
 
-
 });
 
 $(document).on('click','#btnAlerta', function(){
+    /*Validar*/
+    var $validate = true;
 
+    $('.nombrecompleto, documento, celular, correo').hide();
+
+    if($("#nombrecompleto").val()=="" || $("#nombrecompleto").val()==null){
+        $validate = false;
+        $('.nombrecompleto').show();
+    }
+    if($("#documento").val()=="" || $("#documento").val()==null){
+        $validate = false;
+        $('.documento').show();
+    }
+    if($("#celular").val()=="" || $("#celular").val()==null){
+        $validate = false;
+        $('.celular').show();
+    }
+    if($("#correo").val()=="" || $("#correo").val()==null){
+        $validate = false;
+        $('.correo').show();
+    }
+
+
+    if( $validate){
+
+        /*Fecha Nacimiento */
     var $month = $("#cbmeses").val();
-    month = ($month < 10)?  ('0' + $month) : $month;
+    var $day = $("#cbdia").val();
 
-    var fecha = $("#cbanio").val() +"-"+ month +"-"+$("#cbdia").val();
+    $month = ($month < 10)?  ('0' + $month) : $month;
+    $day = ($day < 10)?  ('0' + $day) : $day;
+    var fecha = $("#cbanio").val() +"-"+ $month +"-"+$day;
+
+    /*Fecha de la cita*/
+    var $fechacita =  $('div#datetimepicker12').datepicker('getDate');
+    if($fechacita!=null && $fechacita!='' && $fechacita!=undefined){
+
+        var $yearcita = $fechacita.getFullYear();
+        var $monthcita = $fechacita.getMonth() + 1;
+        var $daycita = $fechacita.getDate();
+
+        $monthcita = ($monthcita < 10)?  ('0' + $monthcita) : $monthcita;
+        $daycita  = ($daycita < 10)?  ('0' + $daycita) : $daycita;
+        $fechacita = $yearcita +"-"+ $monthcita +"-"+$daycita;
+    }
 
     const objct ={
         vNombreCompleto: $("#nombrecompleto").val(),
@@ -53,27 +96,36 @@ $(document).on('click','#btnAlerta', function(){
         dFechaNac: fecha,       
         vCelular: $("#celular").val(),
         vCorreo: $("#correo").val(),
-        dCita: $("#datetimepicker12").val(),       
-
-        // vLugar: document.getElementById('lugar').value,
+        dCita: $fechacita,   
+        vLugar: '',
         Key:"!SDFT$$$$&F(/GF7&F7f))?=0'===IY(&&%$%$!H(U/GFD%VBN(MI YT% %RCGRCVBBUJNU(NN"
     }
-
-
-    console.log(objct);
     
-    // fetch("https://service.poclab.pe/agendarcita/api/cita/PostAgendarCita", {
-    //     method: "POST",
-    //     body: JSON.stringify(objct),
-    //     headers:{
-    //         'Content-Type': 'application/json'
-    //     }
-    // })
-    // .then((res) => res.json())
-    // .then((response) => {
-    //     $('#exampleModal').modal('show');
-    // })
-    // .catch((error) => console.log("Error: ", error));
+    fetch("https://service.poclab.pe/agendarcita/api/cita/PostAgendarCita", {
+        method: "POST",
+        body: JSON.stringify(objct),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((res) => res.json())
+    .then((response) => {
+        $('#exampleModal').modal('show');
+    })
+    .catch((error) => console.log("Error: ", error));
+    }
 });
+
+$(document).on('click','#btnCerrarModal', function(){
+    $("#nombrecompleto").val("");
+    $("#documento").val("");
+    $("#celular").val("");
+    $("#correo").val("");
+    $("#cbdia").val("1");
+    $("#cbmeses").val("1");
+    $("#cbanio").val($aniodesde);
+});
+
+
 
 
