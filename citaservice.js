0035ca740a4f522474fa4d8148fb24f6b6dbc30c
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-08-17 11:17:21 -0500

    merge

--- citaservice.js
+++ citaservice.js
@@ -1,35 +1,5 @@
-onload=function(){
-    /*var ejemplo = 'https://t3.ftcdn.net/jpg/04/19/17/68/360_F_419176802_9s4AoYMfzxDt3kaSYV55whCkTB76NsHN.jpg'
-    //Logos
-    document.getElementById("logoCAMP").src = './assets/logos/logo-CAMP.jpg';
-    document.getElementById("logoPTMS").src = './assets/logos/logo-PTMS-w.png';
-    document.getElementById("logoDONAR").src = './assets/logos/logo-DONAR.jpg';
-    document.getElementById("logoATV").src = './assets/logos/logo-ATV.png';
-    document.getElementById("logoSISA").src = './assets/logos/logo-SA-w.png';
-    document.getElementById("logoHAVAS").src = './assets/logos/logo-HAVAS.png';
-    //Redes
-    document.getElementById("logoFB").src = './assets/redes/facebook.png';
-    document.getElementById("logoIG").src = './assets/redes/instagram.png';
-    document.getElementById("logoIN").src = './assets/redes/linkedin.png';*/
+$(document).on('click','#btnAlerta', function(){
 
-    var sliderImgs = [
-        'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3947459/car.jpg',
-        'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3947459/sunset.jpg',
-        // ejemplo
-    ]
-
-    //Completa slider
-    for (let i = 1; i <= 3; i++) {
-        //El primer hijo es la imagen
-        document.getElementById("slide"+i).children[0].src = sliderImgs[i-1];
-    }
-}
-
-const btnAgendar = document.getElementById("btnAgendar");
-const btnAlerta = document.getElementById("btnAlerta");
-
-
-btnAgendar.addEventListener('click', () => {
     const objct ={
         vNombre: document.getElementById('nombres').value,
         vApellido: document.getElementById('apellidos').value,
@@ -51,12 +21,7 @@ btnAgendar.addEventListener('click', () => {
     })
     .then((res) => res.json())
     .then((response) => {
-        $('#exampleModal').modal(options);
+        $('#exampleModal').modal('show');
     })
     .catch((error) => console.log("Error: ", error));
-});
-
-btnAlerta.addEventListener('click', () => {
-  debugger;
-    $('#exampleModal').modal(options);
 });
