9dc5da6f29ba9eb89c94fb28819bfe5598d4d55c
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-08-17 20:52:22 -0500

    merge

--- js/citaservice.js
+++ js/citaservice.js
@@ -52,9 +52,11 @@ $(document).ready(function(){
 });
 
 $(document).on('click','#btnAlerta', function(){
-    /*Validar*/
-    debugger;
     var $validate = true;
+    var $step1= true;
+    var $step2= true;
+
+
     var $fechavalidar1 = $('div#datetimepicker12').datepicker('getDate');
     var $fechavalidaractual = new Date();
     // var $fechavalidar2 = new Date($fechavalidaractual.getFullYear()+"-"+$fechavalidaractual.getMonth+"-"+ $fechavalidaractual.getDay);
@@ -63,24 +65,29 @@ $(document).on('click','#btnAlerta', function(){
 
     if($("#nombrecompleto").val()=="" || $("#nombrecompleto").val()==null){
         $validate = false;
+        $step1 = false;
         $('.nombrecompleto').show();
     }
     if($("#documento").val()=="" || $("#documento").val()==null){
         $validate = false;
+        $step1 = false;
         $('.documento').show();
     }
     if($("#celular").val()=="" || $("#celular").val()==null){
         $validate = false;
+        $step1 = false;
         $('.celular').show();
     }
     if($("#correo").val()=="" || $("#correo").val()==null){
         $validate = false;
+        $step1 = false;
         $('.correo').show();
     }
     if( $('div#datetimepicker12').datepicker('getDate')=="" ||  
         $('div#datetimepicker12').datepicker('getDate')==null ||  
         $('div#datetimepicker12').datepicker('getDate')==undefined){
             $validate = false;
+            $step2 = false;
             $('#idefechacita').text('la fecha cita es obligatorio');
             $('.datetimepicker12').show();
     }
@@ -96,60 +103,65 @@ $(document).on('click','#btnAlerta', function(){
     //         }
 
     //     }
+    if(!$step1){
+        $('a[href="#step1"]').click();
+    }else if(!$step2){
+        $('a[href="#step2"]').click();
+    }
 
 
-
-    if($validate){
+    if($validate && $step1 && $step2){
 
         /*Fecha Nacimiento */
-    var $month = $("#cbmeses").val();
-    var $day = $("#cbdia").val();
+            var $month = $("#cbmeses").val();
+            var $day = $("#cbdia").val();
 
-    $month = ($month < 10)?  ('0' + $month) : $month;
-    $day = ($day < 10)?  ('0' + $day) : $day;
-    var fecha = $("#cbanio").val() +"-"+ $month +"-"+$day;
+            $month = ($month < 10)?  ('0' + $month) : $month;
+            $day = ($day < 10)?  ('0' + $day) : $day;
+            var fecha = $("#cbanio").val() +"-"+ $month +"-"+$day;
 
-    /*Fecha de la cita*/
-    var $fechacita =  $('div#datetimepicker12').datepicker('getDate');
-    if($fechacita!=null && $fechacita!='' && $fechacita!=undefined){
+        /*Fecha de la cita*/
+            var $fechacita =  $('div#datetimepicker12').datepicker('getDate');
+            if($fechacita!=null && $fechacita!='' && $fechacita!=undefined){
 
-        var $yearcita = $fechacita.getFullYear();
-        var $monthcita = $fechacita.getMonth() + 1;
-        var $daycita = $fechacita.getDate();
+                var $yearcita = $fechacita.getFullYear();
+                var $monthcita = $fechacita.getMonth() + 1;
+                var $daycita = $fechacita.getDate();
 
-        $monthcita = ($monthcita < 10)?  ('0' + $monthcita) : $monthcita;
-        $daycita  = ($daycita < 10)?  ('0' + $daycita) : $daycita;
-        $fechacita = $yearcita +"-"+ $monthcita +"-"+$daycita;
-    }
+                $monthcita = ($monthcita < 10)?  ('0' + $monthcita) : $monthcita;
+                $daycita  = ($daycita < 10)?  ('0' + $daycita) : $daycita;
+                $fechacita = $yearcita +"-"+ $monthcita +"-"+$daycita;
+            }
 
-    const objct ={
-        vNombreCompleto: $("#nombrecompleto").val(),
-        vDocumento: $("#documento").val(),
-        dFechaNac: fecha,       
-        vCelular: $("#celular").val(),
-        vCorreo: $("#correo").val(),
-        dCita: $fechacita,   
-        vLugar: $("#cblugar").val(),
-        Key:"!SDFT$$$$&F(/GF7&F7f))?=0'===IY(&&%$%$!H(U/GFD%VBN(MI YT% %RCGRCVBBUJNU(NN"
-    }
-    
-    console.log(objct);
-    fetch("https://service.poclab.pe/agendarcita/api/cita/PostAgendarCita", {
-        method: "POST",
-        body: JSON.stringify(objct),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    })
-    .then((res) => res.json())
-    .then((response) => {
-        if(response.swt==1){
-            $('#exampleModal').modal('show');
-        }else{
-            $('#exampleModal2').modal('show');
-        }
-    })
-    .catch((error) => console.log("Error: ", error));
+            const objct ={
+                vNombreCompleto: $("#nombrecompleto").val(),
+                vDocumento: $("#documento").val(),
+                dFechaNac: fecha,       
+                vCelular: $("#celular").val(),
+                vCorreo: $("#correo").val(),
+                dCita: $fechacita,   
+                vLugar: $("#cblugar").val(),
+                Key:"!SDFT$$$$&F(/GF7&F7f))?=0'===IY(&&%$%$!H(U/GFD%VBN(MI YT% %RCGRCVBBUJNU(NN"
+            }
+            
+            console.log(objct);
+            fetch("https://service.poclab.pe/agendarcita/api/cita/PostAgendarCita", {
+                method: "POST",
+                body: JSON.stringify(objct),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((res) => res.json())
+            .then((response) => {
+                if(response.swt==1){
+                    $('a[href="#step1"]').click();
+                    $('#exampleModal').modal('show');
+                }else{
+                    $('#exampleModal2').modal('show');
+                }
+            })
+            .catch((error) => console.log("Error: ", error));
     }
 });
 
@@ -170,6 +182,42 @@ $(document).on('keyup', '#documento', function(){
 $(document).on('keyup', '#celular', function(){
     this.value = this.value.replace(/[^0-9]/g,''); 
 });
+
+// ------------step-wizard-------------
+$(document).ready(function () {
+    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        var $target = $(e.target);
+    
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
 
 
 
